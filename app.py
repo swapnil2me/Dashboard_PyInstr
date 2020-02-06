4a0f9251b2e4ee96f58f7a581157c67ec6a65b16
Swapnil More <swapnilmore@iisc.ac.in>
2020-02-06 15:34:23 +0530

    updated app file for db seed

--- app.py
+++ app.py
@@ -26,4 +26,116 @@ def db_drop():
 
 @app.cli.command('db_seed')
 def db_seed():
-    
+    mercury = Planet(planet_name = 'Mercury',
+                     planet_type = 'Class D',
+                     home_star = 'Sol',
+                     mass = 3.258e23,
+                     radius = 1516,
+                     distance = 35.98e6)
+    venus = Planet(planet_name = 'Venus',
+                     planet_type = 'Class K',
+                     home_star = 'Sol',
+                     mass = 4.867e23,
+                     radius = 3760,
+                     distance = 67.24e6)
+    earth = Planet(planet_name = 'Earth',
+                     planet_type = 'Class M',
+                     home_star = 'Sol',
+                     mass = 5.972e23,
+                     radius = 3959,
+                     distance = 92.96e6)
+
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+    db.session.commit()
+
+    test_user = User(first_name = 'William',
+                     last_name = 'Herschel',
+                     email = 'test@test.com',
+                     password = 'password')
+
+    db.session.add(test_user)
+    db.session.commit()
+
+    print('Database Seeded !')
+
+
+@app.route('/')
+def hello_world():
+    return jsonify(message='Hellow World !!')
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message = 'Resource Not Found'), 404
+
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message = name + ': Not old enoug.'), 401
+    else:
+        return jsonify(message = name + ': Welcome.')
+
+
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message = name + ': Not old enoug.'), 401
+    else:
+        return jsonify(message = name + ': Welcome.')
+
+
+@app.route('/planets', methods = ['GET'])
+def planets():
+    planets_list = Planet.query.all()
+    result = planets_schema.dump(planets_list)
+    return jsonify(result)
+
+
+
+# Database Models
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key = True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique = True)
+    password = Column(String)
+
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key = True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = ('planet_id', 'planet_name', 'planet_type', 'home_star',
+                  'mass', 'radius', 'distance')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many = True)
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many = True)
+
+
+if __name__ == '__main__':
+
+    app.run()
+    #db_seed()
